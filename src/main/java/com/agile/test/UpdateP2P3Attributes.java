12db8eca5c37f4fb0440846b959b6afaac3432c9
Depain03 <bdepain@yahoo.com>
2017-11-28 21:15:14 +0530

    first commit

---
+++ src/main/java/com/agile/test/UpdateP2P3Attributes.java
@@ -0,0 +1,43 @@
+package com.agile.test;
+
+import com.agile.api.APIException;
+import com.agile.api.IAgileSession;
+import com.agile.api.ICell;
+import com.agile.api.IDataObject;
+import com.agile.api.IItem;
+import com.agile.api.INode;
+import com.agile.api.ItemConstants;
+import com.agile.px.ActionResult;
+import com.agile.px.ICustomAction;
+
+/**
+ * Hello world!
+ *
+ */
+public class UpdateP2P3Attributes implements ICustomAction
+{
+	public ActionResult doAction(IAgileSession session, INode actionNode, IDataObject object) {
+		ActionResult outcome = null;
+		try{
+			IItem itemName = (IItem)object;
+			System.out.println("Item Name>> " +itemName);
+			
+			ICell addDesc  = (ICell)itemName.getCell(ItemConstants.ATT_PAGE_TWO_TEXT11);
+			addDesc.setValue("Modified Description1");
+			System.out.println(addDesc.getValue());
+			
+			ICell siteInfo = (ICell)itemName.getCell(ItemConstants.ATT_PAGE_THREE_TEXT01);
+			siteInfo.setValue("Modified Site Info1");
+			System.out.println(siteInfo.getValue());
+			outcome = new ActionResult(ActionResult.STRING, "PX Completed");
+		}
+		catch(APIException e){
+			e.printStackTrace();
+		}
+		return outcome;
+	}
+    
+    public static String getHello() {
+    	return "Hello";
+    }
+}
---
+++ src/test/java/com/agile/test/UpdateP2P3AttributesTest.java
@@ -0,0 +1,41 @@
+package com.agile.test;
+
+import com.agile.test.UpdateP2P3Attributes;
+
+import junit.framework.Assert;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class UpdateP2P3AttributesTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+//    public AppTest( String testName )
+//    {
+//        super( testName );
+//    }
+//
+//    /**
+//     * @return the suite of tests being tested
+//     */
+//    public static Test suite()
+//    {
+//        return new TestSuite( AppTest.class );
+//    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testGetHello()
+    {
+        Assert.assertEquals(UpdateP2P3Attributes.getHello(),"Hello");
+    }
+}
